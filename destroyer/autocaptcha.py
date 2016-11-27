127fa27238c86d5d2012bf0d1be8e5136adef47c
BanditArmy <D-7ias4ue1ab42zs@maildrop.cc>
2016-11-27 15:47:38 +1100

    Moved 2captcha code to separate file

---
+++ destroyer/autocaptcha.py
@@ -0,0 +1,104 @@
+import time
+
+import requests
+from settings import user_config, exit_code
+from utils import d_, s_, x_, y_, lb_
+
+
+def get_token_from_2captcha():
+    session = requests.Session()
+    session.verify = False
+    session.cookies.clear()
+    pageurl = 'http://www.{0}'.format(user_config.marketDomain)
+    print (d_(), s_('pageurl'), lb_(pageurl))
+    print (d_(), s_('sitekey'), lb_(user_config.sitekey))
+    while True:
+        data = {
+            'key': user_config.apikey2captcha,
+            'action': 'getbalance',
+            'json': 1,
+        }
+        response = session.get(url='http://2captcha.com/res.php', params=data)
+        try:
+            JSON = response.json()
+        except:
+            raise
+            print (d_(), x_('Response'), y_(response.text))
+            if "ERROR_WRONG_USER_KEY" in response.text:
+                sys.exit(exit_code)
+            print (d_(), x_('Sleeping'), y_(user_config.sleeping, 'seconds'))
+            time.sleep(user_config.sleeping)
+            continue
+
+        if JSON['status'] == 1:
+            balance = JSON['request']
+            print (d_(), s_('Balance'), lb_('${0}'.format(balance)))
+        else:
+            print (d_(), x_('Balance'))
+
+        CAPTCHAID = None
+        proceed = False
+        while not proceed:
+            data = {
+                'key': user_config.apikey2captcha,
+                'method': 'userrecaptcha',
+                'googlekey': user_config.sitekey,
+                'proxy': user_config.proxy2Captcha,
+                'proxytype': 'HTTP',
+                'pageurl': pageurl,
+                'json': 1
+            }
+            response = session.post(url='http://2captcha.com/in.php', data=data)
+            try:
+                JSON = response.json()
+            except:
+                print (d_(), x_('Response'), y_(response.text))
+                print (d_(), x_('Sleeping'), y_(user_config.sleeping, 'seconds'))
+                time.sleep(user_config.sleeping)
+                continue
+
+            if JSON['status'] == 1:
+                CAPTCHAID = JSON['request']
+                proceed = True
+                print (d_(), s_('Captcha ID'), lb_(CAPTCHAID))
+            else:
+                print (d_(), x_('Response'), y_(response.text))
+                print (d_(), x_('Sleeping'), y_(user_config.sleeping, 'seconds'))
+                time.sleep(user_config.sleeping)
+        print (d_(), s_('Waiting'), '%d seconds before polling for Captcha response' % user_config.sleeping)
+        time.sleep(user_config.sleeping)
+
+        TOKEN = None
+        proceed = False
+        while not proceed:
+            data = {
+                'key': user_config.apikey2captcha,
+                'action': 'get',
+                'json': 1,
+                'id': CAPTCHAID,
+            }
+            response = session.get(url='http://2captcha.com/res.php', params=data)
+            JSON = response.json()
+            if JSON['status'] == 1:
+                TOKEN = JSON['request']
+                proceed = True
+                print (d_(), s_('Token ID'), lb_(TOKEN))
+            else:
+                print (d_(), x_('Response'), y_(response.text))
+                print (d_(), x_('Sleeping'), y_(user_config.sleeping, 'seconds'))
+                time.sleep(user_config.sleeping)
+
+        data = {
+            'key': user_config.apikey2captcha,
+            'action': 'getbalance',
+            'json': 1,
+        }
+        response = session.get(url='http://2captcha.com/res.php', params=data)
+        JSON = response.json()
+        if JSON['status'] == 1:
+            balance = JSON['request']
+            print (d_(), s_('Balance'), lb_('${0}'.format(balance)))
+        else:
+            print (d_(), x_('Balance'))
+        if TOKEN is not None:
+            return TOKEN
--- destroyer/destroyer.py
+++ destroyer/destroyer.py
@@ -1,114 +1,17 @@
 import json
-import os
-import random
 import sys
 import time
-from datetime import datetime
 
 import requests
 
+from autocaptcha import get_token_from_2captcha
 from harvester import harvest_tokens_manually
 from settings import exit_code, hypedSkus, captcha_tokens, user_config
 from utils import *
 
+
 # Disable urllib3 warnings
 requests.packages.urllib3.disable_warnings()
-
-
-def getACaptchaTokenFrom2Captcha():
-    session = requests.Session()
-    session.verify = False
-    session.cookies.clear()
-    pageurl = 'http://www.%s' % user_config.marketDomain
-    print (d_(), s_('pageurl'), lb_(pageurl))
-    print (d_(), s_('sitekey'), lb_(user_config.sitekey))
-    while True:
-        data = {
-            'key': user_config.apikey2captcha,
-            'action': 'getbalance',
-            'json': 1,
-        }
-        response = session.get(url='http://2captcha.com/res.php', params=data)
-        try:
-            JSON = json.loads(response.text)
-        except:
-            print (d_(), x_('Response'), y_(response.text))
-            if "ERROR_WRONG_USER_KEY" in response.text:
-                sys.exit(exit_code)
-            print (d_(), x_('Sleeping'), y_(user_config.sleeping, 'seconds'))
-            time.sleep(user_config.sleeping)
-            continue
-
-        if JSON['status'] == 1:
-            balance = JSON['request']
-            print (d_(), s_('Balance'), lb_('${0}'.format(balance)))
-        else:
-            print (d_(), x_('Balance'))
-        CAPTCHAID = None
-        proceed = False
-        while not proceed:
-            data = {
-                'key': user_config.apikey2captcha,
-                'method': 'userrecaptcha',
-                'googlekey': user_config.sitekey,
-                'proxy': user_config.proxy2Captcha,
-                'proxytype': 'HTTP',
-                'pageurl': pageurl,
-                'json': 1
-            }
-            response = session.post(
-                url='http://2captcha.com/in.php', data=data)
-            try:
-                JSON = json.loads(response.text)
-            except:
-                print (d_(), x_('Response'), y_(response.text))
-                print (d_(), x_('Sleeping'), y_(user_config.sleeping, 'seconds'))
-                time.sleep(user_config.sleeping)
-                continue
-
-            if JSON['status'] == 1:
-                CAPTCHAID = JSON['request']
-                proceed = True
-                print (d_(), s_('Captcha ID'), lb_(CAPTCHAID))
-            else:
-                print (d_(), x_('Response'), y_(response.text))
-                print (d_(), x_('Sleeping'), y_(user_config.sleeping, 'seconds'))
-                time.sleep(user_config.sleeping)
-        print (d_(), s_('Waiting'), '%d seconds before polling for Captcha response' % user_config.sleeping)
-        time.sleep(user_config.sleeping)
-        TOKEN = None
-        proceed = False
-        while not proceed:
-            data = {
-                'key': user_config.apikey2captcha,
-                'action': 'get',
-                'json': 1,
-                'id': CAPTCHAID,
-            }
-            response = session.get(url='http://2captcha.com/res.php', params=data)
-            JSON = json.loads(response.text)
-            if JSON['status'] == 1:
-                TOKEN = JSON['request']
-                proceed = True
-                print (d_(), s_('Token ID'), lb_(TOKEN))
-            else:
-                print (d_(), x_('Response'), y_(response.text))
-                print (d_(), x_('Sleeping'), y_(user_config.sleeping, 'seconds'))
-                time.sleep(user_config.sleeping)
-        data = {
-            'key': user_config.apikey2captcha,
-            'action': 'getbalance',
-            'json': 1,
-        }
-        response = session.get(url='http://2captcha.com/res.php', params=data)
-        JSON = json.loads(response.text)
-        if JSON['status'] == 1:
-            balance = JSON['request']
-            print (d_(), s_('Balance'), lb_('${0}'.format(balance)))
-        else:
-            print (d_(), x_('Balance'))
-        if TOKEN is not None:
-            return TOKEN
 
 
 def getClientResponse():
@@ -139,8 +42,7 @@ def getClientResponse():
     if user_config.debug:
         print(d_(), z_('Debug'), o_(clientStockURL))
 
-    response = session.get(url=clientStockURL, headers=headers)
-    return response
+    return session.get(url=clientStockURL, headers=headers)
 
 
 def getVariantResponse():
@@ -376,7 +278,7 @@ def processAddToCart(productInfo):
                            lb_(len(captcha_tokens_reversed)))
                 else:
                     # No manual tokens to pop - so lets use 2captcha
-                    captchaToken = getACaptchaTokenFrom2Captcha()
+                    captchaToken = get_token_from_2captcha()
             addToCartChromeAJAX(pid, captchaToken)
         except KeyboardInterrupt:
             print (d_(), x_('KeyboardInterrupt'))
@@ -504,4 +406,3 @@ def addToCartChromeAJAX(pid, captchaToken):
     # previous size in cart
     browser.delete_all_cookies()
     browser.quit()
-    return
