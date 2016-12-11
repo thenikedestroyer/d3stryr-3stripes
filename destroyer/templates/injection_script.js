61b5826c01fd2ddee3e799e4295723fa00d4a13c
The Nike Destroyer <thenikedestroyer@users.noreply.github.com>
2016-12-11 09:10:25 -0600

    Update injection_script.js

--- destroyer/templates/injection_script.js
+++ destroyer/templates/injection_script.js
@@ -1,3 +1,48 @@
-var url='{{ injection_url }}';
-document.getElementById(document.querySelector("[id^='dwfrm_cart']").id).action = url;
-document.getElementById(document.querySelector("[id^='dwfrm_cart']").id).submit();
+javascript:(function(){
+var f = document.createElement("form");
+f.setAttribute('id',"destroyer");
+f.setAttribute('method',"post");
+f.setAttribute('action',"{{ atc_url }}");
+
+var masterPid = document.createElement("input");
+masterPid.setAttribute('type',"hidden");
+masterPid.setAttribute('name',"masterPid");
+masterPid.setAttribute('value',"{{ master_pid }}");
+
+var pid = document.createElement("input");
+pid.setAttribute('type',"hidden");
+pid.setAttribute('name',"pid");
+pid.setAttribute('value',"{{ pid }}");
+
+var ajaxOption = document.createElement("input");
+ajaxOption.setAttribute('type',"hidden");
+ajaxOption.setAttribute('name',"ajax");
+ajaxOption.setAttribute('value',"true");
+
+var responseOption = document.createElement("input");
+responseOption.setAttribute('type',"hidden");
+responseOption.setAttribute('name',"responseformat");
+responseOption.setAttribute('value',"json");
+
+var sessionSelectedStoreID = document.createElement("input");
+sessionSelectedStoreID.setAttribute('type',"hidden");
+sessionSelectedStoreID.setAttribute('name',"sessionSelectedStoreID");
+sessionSelectedStoreID.setAttribute('value',"null");
+
+var s = document.createElement("input");
+s.setAttribute('type',"submit");
+s.setAttribute('value',"Submit");
+
+f.appendChild(masterPid);
+f.appendChild(pid);
+f.appendChild(ajaxOption);
+f.appendChild(responseOption);
+f.appendChild(sessionSelectedStoreID);
+f.appendChild(s);
+
+document.getElementsByTagName('body')[0].appendChild(f);
+
+})();
+
+document.getElementById(document.querySelector("[id^='destroyer']").id).submit();
+
